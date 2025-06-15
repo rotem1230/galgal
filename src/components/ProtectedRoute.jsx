598fcd659f5f68a9e7b618805b7dd9a756fe0489
rotem1230 <rotem12510@gmail.com>
2025-06-15 20:41:47 +0300

    תיקון פונקציונליות 'זכור אותי' בשני הפורטלים

--- src/components/ProtectedRoute.jsx
+++
@@ -1,5 +0,0 @@
-// בדיקה אם המשתמש הוא מנהל
-if (currentUser.email === 'rotemhaha321@gmail.com' || currentUser.email === 'kfirgal505@gmail.com') {
-  console.log(`זיהינו את המשתמש המיוחד ${currentUser.email} - מאפשר גישה`);
-  return children;
-} 
